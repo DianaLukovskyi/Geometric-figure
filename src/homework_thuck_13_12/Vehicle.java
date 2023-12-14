ed581f5f2542b6db894f87dddcf1d8b1a64fdd3d
DianaLukovskyi <151841443+DianaLukovskyi@users.noreply.github.com>
2023-12-14 19:49:41 +0300

    Homework peredelka Truck 13/12/23 s roditelskim i dochernim klasom

---
+++ src/homework_thuck_13_12/Vehicle.java
@@ -0,0 +1,56 @@
+package homework_thuck_13_12;
+
+public class Vehicle {
+    private String model;
+    private String manufacturer;
+    private int year;
+    private String color;
+    private String owner;
+
+    public Vehicle(String model, String manufacturer, int year, String color, String owner) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.color = color;
+        this.owner = owner;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void move() {
+        System.out.println("A car  moves");
+    }
+
+    public void stop() {
+        System.out.println("A car stops");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                '}';
+
+    }
+}
